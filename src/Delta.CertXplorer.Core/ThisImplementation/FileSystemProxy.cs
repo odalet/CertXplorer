d0059be34c5cdf35a4d0c888df683f14df66897c
odalet <odalet@gmail.com>
2021-03-03 22:01:12 +0100

    Code Quality

--- src/Delta.CertXplorer.Core/ThisImplementation/FileSystemProxy.cs
+++ src/Delta.CertXplorer.Core/ThisImplementation/FileSystemProxy.cs
@@ -12,139 +12,76 @@ namespace Delta.CertXplorer.ThisImplementation
     /// </remarks>
     public class FileSystemProxy
     {
-        private SpecialDirectoriesProxy specialDirectoriesProxy = null;
+        public SpecialDirectoriesProxy SpecialDirectories { get; } = new SpecialDirectoriesProxy();
 
-        /// <summary>Gets the name of a new temporary file.</summary>
-        /// <returns>The name of a new temporary file.</returns>
-        public string GetTempFileName() { return Path.GetTempFileName(); }
+        public string GetTempFileName() => Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
 
-        /// <summary>
-        /// Gets or sets the current directory.
-        /// </summary>
-        /// <value>The current directory.</value>
         public string CurrentDirectory
         {
-            get { return NormalizePath(Directory.GetCurrentDirectory()); }
-            set { Directory.SetCurrentDirectory(value); }
+            get => NormalizePath(Directory.GetCurrentDirectory());
+            set => Directory.SetCurrentDirectory(value);
         }
 
-        /// <summary>
-        /// Gets the special directories list.
-        /// </summary>
-        /// <value>The special directories.</value>
-        public SpecialDirectoriesProxy SpecialDirectories
-        {
-            get
+        internal static string NormalizePath(string path) => GetLongPath(RemoveEndingSeparator(Path.GetFullPath(path)));
+
+        private static string RemoveEndingSeparator(string path) => 
+            Path.IsPathRooted(path) && path.Equals(Path.GetPathRoot(path), StringComparison.OrdinalIgnoreCase) ?
+            path : 
+            path.TrimEnd(new char[]
             {
-                if (specialDirectoriesProxy == null)
-                    specialDirectoriesProxy = new SpecialDirectoriesProxy();
-                return specialDirectoriesProxy;
-            }
-        }
-
-        #region Private implementation
-
-        /// <summary>
-        /// Normalizes the path.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        /// <returns>normalized path.</returns>
-        internal static string NormalizePath(string path)
-        {
-            return GetLongPath(RemoveEndingSeparator(Path.GetFullPath(path)));
-        }
-
-        /// <summary>Removes the ending separator of a path.</summary>
-        /// <param name="path">The path.</param>
-        /// <returns>normalized path.</returns>
-        private static string RemoveEndingSeparator(string path)
-        {
-            if (Path.IsPathRooted(path) &&
-                path.Equals(Path.GetPathRoot(path), StringComparison.OrdinalIgnoreCase))
-                return path;
-
-            return path.TrimEnd(new char[] 
-            { 
-                Path.DirectorySeparatorChar, 
-                Path.AltDirectorySeparatorChar 
+                Path.DirectorySeparatorChar,
+                Path.AltDirectorySeparatorChar
             });
-        }
 
-        /// <summary>
-        /// Determines whether the specified path is a root path.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        /// <returns>
-        /// 	<c>true</c> if the specified path is a root path; otherwise, <c>false</c>.
-        /// </returns>
         private static bool IsRoot(string path)
         {
             if (!Path.IsPathRooted(path)) return false;
 
-            path = path.TrimEnd(new char[] 
-            { 
-                Path.DirectorySeparatorChar, 
-                Path.AltDirectorySeparatorChar 
+            path = path.TrimEnd(new char[]
+            {
+                Path.DirectorySeparatorChar,
+                Path.AltDirectorySeparatorChar
             });
 
-            return (string.Compare(
-                path,
-                Path.GetPathRoot(path),
-                StringComparison.OrdinalIgnoreCase) == 0);
+            return string.Compare(path, Path.GetPathRoot(path), StringComparison.OrdinalIgnoreCase) == 0;
         }
 
-        /// <summary>
-        /// Gets the long version of a path.
-        /// </summary>
-        /// <param name="fullPath">The full path.</param>
-        /// <returns>normalized path.</returns>
         private static string GetLongPath(string fullPath)
         {
             try
             {
                 if (!IsRoot(fullPath))
                 {
-                    DirectoryInfo info = new DirectoryInfo(GetParentPath(fullPath));
+                    var info = new DirectoryInfo(GetParentPath(fullPath));
                     if (File.Exists(fullPath))
                         return info.GetFiles(Path.GetFileName(fullPath))[0].FullName;
 
                     if (Directory.Exists(fullPath))
                         return info.GetDirectories(Path.GetFileName(fullPath))[0].FullName;
                 }
+
                 return fullPath;
             }
             catch (Exception exception)
             {
-                if (!(exception is ArgumentException) &&
-                    !(exception is ArgumentNullException) &&
-                    !(exception is PathTooLongException) &&
-                    !(exception is NotSupportedException) &&
-                    !(exception is DirectoryNotFoundException) &&
-                    !(exception is SecurityException) &&
-                    !(exception is UnauthorizedAccessException)) throw;
+                if (exception is not ArgumentException &&
+                    exception is not ArgumentNullException &&
+                    exception is not PathTooLongException &&
+                    exception is not NotSupportedException &&
+                    exception is not DirectoryNotFoundException &&
+                    exception is not SecurityException &&
+                    exception is not UnauthorizedAccessException) throw;
 
                 return fullPath;
             }
         }
 
-        /// <summary>
-        /// Gets the parent path of <paramref name="path"/>.
-        /// </summary>
-        /// <param name="path">The path.</param>
-        /// <returns>The path's parent.</returns>
-        private static string GetParentPath(string path)
-        {
-            if (IsRoot(path)) throw new ArgumentException(string.Format(
-                "Can't get parent path for{0}: it is a root path.", path), "path");
-
-            return Path.GetDirectoryName(
-                path.TrimEnd(new char[] 
-                { 
-                    Path.DirectorySeparatorChar, 
-                    Path.AltDirectorySeparatorChar 
-                }));
-        }
-
-        #endregion
+        private static string GetParentPath(string path) => IsRoot(path) ?
+            throw new ArgumentException($"Cannot get parent path for {path}: it is a root path") :
+            Path.GetDirectoryName(path.TrimEnd(new char[]
+            {
+                Path.DirectorySeparatorChar,
+                Path.AltDirectorySeparatorChar
+            }));
     }
 }
